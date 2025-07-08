39c73600d9e475a5a6934253f92572ef23c039fb
GoopyGoop1689 <goowenzhan@gmail.com>
2025-07-08 17:27:41 +0800

    8th July
    adding functions to buttons

--- src/main/scala-3/ch/makery/address/MainApp.scala
+++ src/main/scala-3/ch/makery/address/MainApp.scala
@@ -1,17 +1,18 @@
 package ch.makery.address
 
 import ch.makery.address.model.Person
+import ch.makery.address.view.PersonEditDialogController
 import javafx.fxml.FXMLLoader
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
-import scalafx.scene.Scene
 import scalafx.Includes.*
 import javafx.scene as jfxs
-
 import scalafx.beans.property
-
 import scalafx.beans.property.StringProperty
 import scalafx.collections.ObservableBuffer
+import scalafx.scene.Scene
+import scalafx.stage.{Modality, Stage}
+
 
 object MainApp extends JFXApp3:
 
@@ -85,3 +86,21 @@ object MainApp extends JFXApp3:
     def area: Double = 3.142 * value * value
     
   println(5.area)
+
+  def showPersonEditDialog(person: Person): Boolean =
+    val resource = getClass.getResource("view/PersonEditDialog.fxml")
+    val loader = new FXMLLoader(resource)
+    loader.load();
+    val roots2 = loader.getRoot[jfxs.Parent]
+    val control = loader.getController[PersonEditDialogController]
+
+    val dialog = new Stage():
+      initModality(Modality.ApplicationModal)
+      initOwner(stage)
+      scene = new Scene:
+        root = roots2
+
+    control.dialogStage = dialog
+    control.person = person
+    dialog.showAndWait()
+    control.okClicked
